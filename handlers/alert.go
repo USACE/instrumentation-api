e8625246ba717b93ae86240f0750518ed250de50
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-09-23 22:15:45 -0500

    initial alert subscription endpoints; update regression tests

--- handlers/alert.go
+++ handlers/alert.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/USACE/instrumentation-api/models"
+	"github.com/lib/pq"
 
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
@@ -61,7 +62,7 @@ func CreateInstrumentAlert(db *sqlx.DB) echo.HandlerFunc {
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, err)
 		}
-		// Send Alertsg
+		// Send Alerts
 		return c.JSON(http.StatusCreated, aa)
 	}
 }
@@ -113,5 +114,114 @@ func DeleteInstrumentAlert(db *sqlx.DB) echo.HandlerFunc {
 			return c.JSON(http.StatusInternalServerError, err)
 		}
 		return c.NoContent(http.StatusOK)
+	}
+}
+
+// SubscribeProfileToInstrumentAlert subscribes a profile to an alert
+func SubscribeProfileToInstrumentAlert(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		p, err := myProfileFromContext(c, db)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		profileID := p.ID
+
+		alertID, err := uuid.Parse(c.Param("alert_id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		pa, err := models.SubscribeProfileToInstrumentAlert(db, &alertID, &profileID)
+		if err != nil {
+			if err, ok := err.(*pq.Error); ok {
+				switch err.Code {
+				// Profile already subscribed to instrument alert; Get ProfileAlert and return it
+				// Return a RESTful 200; i.e. nothing wrong, state of data is already "subscribed"
+				case "23505":
+					pa, err := models.GetAlertSubscription(db, &alertID, &profileID)
+					if err != nil {
+						return c.JSON(http.StatusInternalServerError, err)
+					}
+					return c.JSON(http.StatusOK, &pa)
+				default:
+					return c.JSON(http.StatusInternalServerError, err)
+				}
+			}
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		return c.JSON(http.StatusOK, &pa)
+	}
+}
+
+// UnsubscribeProfileToInstrumentAlert unsubscribes a profile to an alert
+func UnsubscribeProfileToInstrumentAlert(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		p, err := myProfileFromContext(c, db)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		profileID := p.ID
+
+		alertID, err := uuid.Parse(c.Param("alert_id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		err = models.UnsubscribeProfileToInstrumentAlert(db, &alertID, &profileID)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		return c.NoContent(http.StatusOK)
+	}
+}
+
+// ListMyAlertSubscriptions returns all alerts you are subscribed to and settings
+func ListMyAlertSubscriptions(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		p, err := myProfileFromContext(c, db)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		profileID := p.ID
+		ss, err := models.ListMyAlertSubscriptions(db, &profileID)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		return c.JSON(http.StatusOK, &ss)
+	}
+}
+
+// UpdateMyAlertSubscription updates settings for an alert subscription
+func UpdateMyAlertSubscription(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var s models.AlertSubscription
+		if err := c.Bind(&s); err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		// AlertID From Route Params
+		sID, err := uuid.Parse(c.Param("alert_subscription_id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		if s.ID != sID {
+			return c.String(http.StatusBadRequest, "route parameter subscription_id does not match id in JSON payload")
+		}
+		// Get Profile
+		p, err := myProfileFromContext(c, db)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		// Verify Profile ID matches ProfileID of Subscription to be Modified
+		// No Modifying anyone else's settings
+		t, err := models.GetAlertSubscriptionByID(db, &sID)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+		if p.ID != t.ProfileID {
+			return c.NoContent(http.StatusUnauthorized)
+		}
+		sUpdated, err := models.UpdateMyAlertSubscription(db, &s)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		return c.JSON(http.StatusOK, sUpdated)
 	}
 }
--- models/alert.go
+++ models/alert.go
@@ -18,6 +18,33 @@ type Alert struct {
 	AuditInfo
 }
 
+// AlertSubscription is a profile subscription to an alert
+type AlertSubscription struct {
+	ID        uuid.UUID `json:"id"`
+	AlertID   uuid.UUID `json:"alert_id" db:"alert_id"`
+	ProfileID uuid.UUID `json:"profile_id" db:"profile_id"`
+	AlertSubscriptionSettings
+}
+
+// AlertSubscriptionSettings holds all settings for an AlertSubscription
+type AlertSubscriptionSettings struct {
+	MuteUI     bool `json:"mute_ui" db:"mute_ui"`
+	MuteNotify bool `json:"mute_notify" db:"mute_notify"`
+}
+
+// AlertSubscriptionCollection is a collection of AlertSubscription items
+type AlertSubscriptionCollection struct {
+	Items []AlertSubscription `json:"items"`
+}
+
+// EmailAlert is an email subscription to an alert
+type EmailAlert struct {
+	ID         uuid.UUID `json:"id"`
+	AlertID    uuid.UUID `json:"alert_id"`
+	EmailID    uuid.UUID `json:"profile_id"`
+	MuteNotify bool      `json:"mute_notify" db:"mute_notify"`
+}
+
 // AlertCollection holds one ore more alert items
 type AlertCollection struct {
 	Items []Alert `json:"items"`
@@ -39,6 +66,26 @@ func (c *AlertCollection) UnmarshalJSON(b []byte) error {
 		c.Items = []Alert{a}
 	default:
 		c.Items = make([]Alert, 0)
+	}
+	return nil
+}
+
+// UnmarshalJSON implements the UnmarshalJSON Interface for AlertSubscription
+func (c *AlertSubscriptionCollection) UnmarshalJSON(b []byte) error {
+
+	switch JSONType(b) {
+	case "ARRAY":
+		if err := json.Unmarshal(b, &c.Items); err != nil {
+			return err
+		}
+	case "OBJECT":
+		var a AlertSubscription
+		if err := json.Unmarshal(b, &a); err != nil {
+			return err
+		}
+		c.Items = []AlertSubscription{a}
+	default:
+		c.Items = make([]AlertSubscription, 0)
 	}
 	return nil
 }
@@ -141,10 +188,68 @@ func DeleteInstrumentAlert(db *sqlx.DB, alertID *uuid.UUID, instrumentID *uuid.U
 	return nil
 }
 
-// SubscribeAlertProfile
+// SubscribeProfileToInstrumentAlert subscribes a profile to an instrument alert
+func SubscribeProfileToInstrumentAlert(db *sqlx.DB, alertID *uuid.UUID, profileID *uuid.UUID) (*AlertSubscription, error) {
+	var pa AlertSubscription
+	err := db.QueryRowx(
+		`INSERT INTO profile_alerts (alert_id, profile_id) VALUES ($1, $2) RETURNING *`, alertID, profileID,
+	).StructScan(&pa)
+	if err != nil {
+		return nil, err
+	}
+	return &pa, nil
+}
 
-// UnsubscribeAlertProfile
+// UnsubscribeProfileToInstrumentAlert subscribes a profile to an instrument alert
+func UnsubscribeProfileToInstrumentAlert(db *sqlx.DB, alertID *uuid.UUID, profileID *uuid.UUID) error {
+	if _, err := db.Exec(
+		`DELETE FROM profile_alerts WHERE alert_id = $1 AND profile_id = $2`, alertID, profileID,
+	); err != nil {
+		return err
+	}
+	return nil
+}
 
-// SubscribeAlertEmail
+// GetAlertSubscription returns a AlertSubscription
+func GetAlertSubscription(db *sqlx.DB, alertID *uuid.UUID, profileID *uuid.UUID) (*AlertSubscription, error) {
+	var pa AlertSubscription
+	if err := db.Get(
+		&pa, `SELECT * FROM profile_alerts WHERE alert_id = $1 AND profile_id = $2`, alertID, profileID,
+	); err != nil {
+		return nil, err
+	}
+	return &pa, nil
+}
 
-// UnsubscribeAlertEmail
+// GetAlertSubscriptionByID returns an alert subscription
+func GetAlertSubscriptionByID(db *sqlx.DB, id *uuid.UUID) (*AlertSubscription, error) {
+	var s AlertSubscription
+	if err := db.Get(&s, `SELECT * FROM profile_alerts WHERE id = $1`, id); err != nil {
+		return nil, err
+	}
+	return &s, nil
+}
+
+// ListMyAlertSubscriptions returns all profile_alerts for a given profile ID
+func ListMyAlertSubscriptions(db *sqlx.DB, profileID *uuid.UUID) ([]AlertSubscription, error) {
+	ss := make([]AlertSubscription, 0)
+	if err := db.Select(
+		&ss, `SELECT * FROM profile_alerts WHERE profile_id = $1`, profileID,
+	); err != nil {
+		return make([]AlertSubscription, 0), err
+	}
+	return ss, nil
+}
+
+// UpdateMyAlertSubscription updates properties on a AlertSubscription
+func UpdateMyAlertSubscription(db *sqlx.DB, s *AlertSubscription) (*AlertSubscription, error) {
+
+	_, err := db.Exec(
+		"UPDATE profile_alerts SET mute_ui=$1, mute_notify=$2 WHERE alert_id=$3 AND profile_id=$4",
+		s.MuteUI, s.MuteNotify, s.AlertID, s.ProfileID,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return GetAlertSubscription(db, &s.AlertID, &s.ProfileID)
+}
