792cf559390410731dcbcfcd31533fd4fb0b6b7d
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-10-27 15:48:29 -0500

    add lrc projects

---
+++ database/data_lrc.sql
@@ -0,0 +1,19 @@
+INSERT INTO project (name, slug) VALUES
+    ('Cedars Lock and Dam', 'cedars-lock-and-dam'),
+    ('Chicago River and Harbor Controlling Works', 'chicago-river-and-harbor-controlling-works'),
+    ('Depere Lock and Dam', 'depere-lock-and-dam'),
+    ('J. Edward Roush Dam', 'j-edward-roush-dam'),
+    ('J. Edward Roush Dam - Markle Levee', 'j-edward-roush-dam-markle-levee'),
+    ('Kaukauna Locks and Dam', 'kaukauna-locks-and-dam'),
+    ('Little Chute Locks and Dam', 'little-chute-locks-and-dam'),
+    ('Little Kaukauna Lock and Dam', 'little-kaukauna-lock-and-dam'),
+    ('Lockport Lock and Controlling Works', 'lockport-lock-and-controlling-works'),
+    ('Lower Appleton Locks and Dam', 'lower-appleton-locks-and-dam'),
+    ('Menasha Lock and Dam', 'menasha-lock-and-dam'),
+    ('Mississinewa Dam', 'mississinewa-dam'),
+    ('Rapide Croche Lock and Dam', 'rapide-croche-lock-and-dam'),
+    ('Salamonie Dam', 'salamonie-dam'),
+    ('Thomas J. O''brien Lock and Controlling Works', 'thomas-j-obrien-lock-and-controlling-works'),
+    ('Upper Appleton Locks and Dam', 'upper-appleton-locks-and-dam'),
+    ('Chicago Sanitarty and Ship Canal Dispersal Barrier', 'chicago-sanitary-and-ship-canal-dispersal-barrier');
+
