f092f8112598ccef20e3a6ba31bc8bc058e244de
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-01-25 13:28:55 -0600

    endpoint /instrumentation/aware/data_acquisition_config

--- database/sql/11-tables_aware.sql
+++ database/sql/11-tables_aware.sql
@@ -37,8 +37,23 @@ CREATE OR REPLACE VIEW v_aware_platform_parameter_enabled AS (
 );
 
 -- Seed Data
+-- aware_parameter
 INSERT INTO aware_parameter (id, key, parameter_id, unit_id) VALUES
     ('1d9f9d06-6fcb-41dd-9fe4-e513a2575e74', 'depth1', '068b59b0-aafb-4c98-ae4b-ed0365a6fbac', '4ee79a3d-a053-41b8-85b5-bb2eea3c9d1a'),
     ('c5f2842d-a5a9-4f53-9583-f613080a9c36', 'battery', '430e5edb-e2b5-4f86-b19f-cda26a27e151', '6b5bd788-8c78-43bb-b5a3-ad544b858a64'),
     ('78d32638-5137-481c-aa9d-a48c2d57824a', 'baro', '1de79e29-fb70-45c3-ae7d-4695517ced90', '55dda9ef-7ba6-4432-b64d-8ef0e65154f4'),
     ('53ce89a7-1db8-45bd-baf9-9536d75d7046', 'h2oTemp', 'de6112da-8489-4286-ae56-ec72aa09974d', '6462733b-5b42-46a2-ad44-882a5332eafc');
+
+-- aware_platform
+-- aware_id used below is not an actual aware_id; generated for testing
+INSERT INTO aware_platform (id, aware_id, instrument_id) VALUES
+    ('b896ce34-2bd4-436c-9f28-7a1eefb5744a', '6df213c4-a582-4735-a916-6f4065082872', 'a7540f69-c41e-43b3-b655-6e44097edb7e');
+
+-- enable all parameters for sample aware platform
+INSERT INTO aware_platform_parameter_enabled (aware_platform_id, aware_parameter_id) (
+	SELECT a.id AS aware_platform_id,
+		   b.id AS aware_parameter_id
+	FROM aware_platform a
+	CROSS JOIN aware_parameter b
+	ORDER BY aware_platform_id
+);
--- models/aware.go
+++ models/aware.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
@@ -10,6 +12,24 @@ type AwareParameter struct {
 	Key         string    `json:"key"`
 	ParameterID uuid.UUID `json:"parameter_id" db:"parameter_id"`
 	UnitID      uuid.UUID `json:"unit_id" db:"unit_id"`
+}
+
+// AwarePlatformParameterConfig holds information about which parameters are "enabled" for given instrument(s)
+// { projectID: <uuid4>, instrument_id: <uuid4>, aware_platform_id: <uuid4>, aware_parameters: { <string>: <uuid4> } }
+// aware_parameters is a map of <aware_parameter_key> : <timeseries_id>
+type AwarePlatformParameterConfig struct {
+	ProjectID       uuid.UUID             `json:"project_id" db:"project_id"`
+	InstrumentID    uuid.UUID             `json:"instrument_id" db:"instrument_id"`
+	AwarePlatformID uuid.UUID             `json:"aware_platform_id" db:"aware_platform_id"`
+	AwareParameters map[string]*uuid.UUID `json:"aware_parameters"`
+}
+
+type awarePlatformParameterEnabled struct {
+	ProjectID         uuid.UUID  `json:"project_id" db:"project_id"`
+	InstrumentID      uuid.UUID  `json:"instrument_id" db:"instrument_id"`
+	AwarePlatformID   uuid.UUID  `json:"aware_platform_id" db:"aware_platform_id"`
+	AwareParameterKey string     `json:"aware_parameter_key" db:"aware_parameter_key"`
+	TimeseriesID      *uuid.UUID `json:"timeseries_id" db:"timeseries_id"`
 }
 
 func ListAwareParameters(db *sqlx.DB) ([]AwareParameter, error) {
@@ -22,18 +42,51 @@ func ListAwareParameters(db *sqlx.DB) ([]AwareParameter, error) {
 	return pp, nil
 }
 
-// func ListAwareEnabled(db *sqlx.DB) error {
-// 	sql := `
-// 	SELECT i.project_id          AS project_id,
-// 	       i.id                  AS instrument_id,
-// 	       e.aware_platform_id   AS platform_id,
-// 	       e.key                 AS aware_parameter_key,
-// 	       t.id                  AS timeseries_id
-//     FROM aware_platform_parameter_enabled e
-//     INNER JOIN aware_platform a ON a.platform_id = e.aware_platform_id
-//     INNER JOIN instrument i ON i.id = a.instrument_id
-//     INNER JOIN aware_parameter b ON b.id = e.aware_parameter_id
-// 	LEFT JOIN timeseries t ON t.instrument_id=i.id, t.parameter_id=b.parameter_id, t.unit_id=b.parameter_id   `
+func listAwarePlatformParameterEnabled(db *sqlx.DB) ([]awarePlatformParameterEnabled, error) {
 
-// 	return nil
-// }
+	sql := `SELECT i.project_id          AS project_id,
+	               i.id                  AS instrument_id,
+				   e.aware_platform_id   AS aware_platform_id,
+				   b.key                 AS aware_parameter_key,
+				   t.id                  AS timeseries_id
+            FROM aware_platform_parameter_enabled e
+            INNER JOIN aware_platform a ON a.id = e.aware_platform_id
+            INNER JOIN instrument i ON i.id = a.instrument_id
+            INNER JOIN aware_parameter b ON b.id = e.aware_parameter_id
+			LEFT JOIN timeseries t ON t.instrument_id=i.id AND t.parameter_id=b.parameter_id AND t.unit_id=b.unit_id
+			ORDER BY e.aware_platform_id, b.key`
+	aa := make([]awarePlatformParameterEnabled, 0)
+	if err := db.Select(&aa, sql); err != nil {
+		return make([]awarePlatformParameterEnabled, 0), err
+	}
+	return aa, nil
+}
+
+func ListAwarePlatformParameterConfig(db *sqlx.DB) ([]AwarePlatformParameterConfig, error) {
+	ee, err := listAwarePlatformParameterEnabled(db)
+	fmt.Println(len(ee))
+	if err != nil {
+		return make([]AwarePlatformParameterConfig, 0), err
+	}
+	// reorganize aware_parameter_key, timeseries_id into map for each instrument
+	// Map of aware parameters to timeseries
+	m1 := make(map[uuid.UUID]AwarePlatformParameterConfig)
+	for _, e := range ee {
+		if _, ok := m1[e.InstrumentID]; !ok {
+			m1[e.InstrumentID] = AwarePlatformParameterConfig{
+				ProjectID:       e.ProjectID,
+				InstrumentID:    e.InstrumentID,
+				AwarePlatformID: e.AwarePlatformID,
+				AwareParameters: make(map[string]*uuid.UUID),
+			}
+		}
+		m1[e.InstrumentID].AwareParameters[e.AwareParameterKey] = e.TimeseriesID
+	}
+
+	cc := make([]AwarePlatformParameterConfig, 0)
+	for k := range m1 {
+		cc = append(cc, m1[k])
+	}
+	return cc, nil
+
+}
