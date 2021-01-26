7961fc33c85e256ef9ecb40b4527e505d7c68a75
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-01-25 20:21:31 -0600

    modify view

--- database/sql/11-tables_aware.sql
+++ database/sql/11-tables_aware.sql
@@ -25,17 +25,17 @@ CREATE TABLE IF NOT EXISTS public.aware_platform_parameter_enabled (
 );
 
 CREATE OR REPLACE VIEW v_aware_platform_parameter_enabled AS (
-    SELECT i.project_id          AS project_id,
-        i.id                  AS instrument_id,
-        e.aware_platform_id   AS platform_id,
-        b.key                 AS aware_parameter_key,
-        t.id                  AS timeseries_id
+    SELECT i.project_id  AS project_id,
+	       i.id          AS instrument_id,
+		   a.aware_id    AS aware_id,
+		   b.key         AS aware_parameter_key,
+		   t.id          AS timeseries_id
     FROM aware_platform_parameter_enabled e
     INNER JOIN aware_platform a ON a.id = e.aware_platform_id
     INNER JOIN instrument i ON i.id = a.instrument_id
     INNER JOIN aware_parameter b ON b.id = e.aware_parameter_id
-    LEFT JOIN timeseries t ON t.instrument_id=i.id AND t.parameter_id=b.parameter_id AND t.unit_id=b.unit_id
-    ORDER BY project_id, platform_id
+	LEFT JOIN timeseries t ON t.instrument_id=i.id AND t.parameter_id=b.parameter_id AND t.unit_id=b.unit_id
+	ORDER BY i.project_id, a.aware_id
 );
 
 -- Seed Data
