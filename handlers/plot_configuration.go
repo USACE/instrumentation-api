8ca3eadd45c88a9abac2e8a26ceff0ec85a24f1e
Jeff Gregory <37386268+jeffsuperglide@users.noreply.github.com>
2021-02-23 20:12:36 -0600

    Additional functions for plot configurations

--- handlers/plot_configuration.go
+++ handlers/plot_configuration.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/USACE/instrumentation-api/dbutils"
 	"github.com/USACE/instrumentation-api/models"
 	"github.com/google/uuid"
 
@@ -19,8 +21,7 @@ func ListPlotConfigurations(db *sqlx.DB) echo.HandlerFunc {
 		}
 		cc, err := models.ListPlotConfigurations(db, &pID)
 		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
-			// return c.JSON(http.StatusInternalServerError, err)
+			return c.JSON(http.StatusInternalServerError, err)
 		}
 		return c.JSON(http.StatusOK, &cc)
 	}
@@ -29,14 +30,125 @@ func ListPlotConfigurations(db *sqlx.DB) echo.HandlerFunc {
 // GetPlotConfiguration returns single instrument group
 func GetPlotConfiguration(db *sqlx.DB) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		id, err := uuid.Parse(c.Param("plot_configuration_id"))
+		// Project ID
+		pID, err := uuid.Parse(c.Param("project_id"))
 		if err != nil {
 			return c.String(http.StatusBadRequest, "Malformed ID")
 		}
-		g, err := models.GetPlotConfiguration(db, id)
+		// Plot configuration ID
+		cID, err := uuid.Parse(c.Param("plot_configuration_id"))
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, err)
+			return c.String(http.StatusBadRequest, "Malformed ID")
+		}
+		// Get the plot configuration
+		g, err := models.GetPlotConfiguration(db, pID, cID)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
 		}
 		return c.JSON(http.StatusOK, g)
+	}
+}
+
+// CreatePlotConfiguration add plot configuration for a project
+func CreatePlotConfiguration(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// Sanatized instruments with ID, PID, and slug assigned
+		newPlotConfigCollection := func(c echo.Context) (models.PlotConfigurationCollection, error) {
+			pc := models.PlotConfigurationCollection{}
+			if err := c.Bind(&pc); err != nil {
+				return models.PlotConfigurationCollection{}, err
+			}
+
+			// Get PID of Instruments
+			projectID, err := uuid.Parse(c.Param("project_id"))
+			if err != nil {
+				return models.PlotConfigurationCollection{}, err
+			}
+
+			// slugs already taken in the database
+			slugsTaken, err := models.ListInstrumentSlugs(db)
+			if err != nil {
+				return models.PlotConfigurationCollection{}, err
+			}
+
+			// profile of user creating instruments
+			p := c.Get("profile").(*models.Profile)
+
+			// timestamp
+			t := time.Now()
+
+			for idx := range pc.Items {
+				// Assign projectID
+				pc.Items[idx].ProjectID = &projectID
+				// Assign Slug
+				s, err := dbutils.NextUniqueSlug(pc.Items[idx].Name, slugsTaken)
+				if err != nil {
+					return models.PlotConfigurationCollection{}, err
+				}
+				pc.Items[idx].Slug = s
+				// Assign Creator
+				pc.Items[idx].Creator = p.ID
+				// Assign CreateDate
+				pc.Items[idx].CreateDate = t
+				// Add slug to array of slugs originally fetched from the database
+				// to catch duplicate names/slugs from the same bulk upload
+				slugsTaken = append(slugsTaken, s)
+			}
+
+			return pc, nil
+		}
+
+		// Plot Configurations
+		pc, err := newPlotConfigCollection(c)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err)
+		}
+
+		// // Validate POST
+		// if c.QueryParam("dry_run") == "true" {
+		// 	v, err := models.ValidateCreateInstruments(db, pc.Items)
+		// 	if err != nil {
+		// 		return c.JSON(http.StatusBadRequest, err)
+		// 	}
+		// 	return c.JSON(http.StatusOK, v)
+		// }
+
+		// Actually POST
+		nn, err := models.CreatePlotConfiguration(db, pc.Items)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		return c.JSON(http.StatusCreated, nn)
+	}
+}
+
+// // UpdatePlotConfiguration update plot configuration for a project
+// func UpdatePlotConfiguration() echo.HandlerFunc {
+// 	if err != nil {
+// 		return c.JSON(http.StatusBadRequest, err.Error())
+// 	}
+// 	cc, err := models.UpdatePlotConfiguration(db, &pID)
+// 	if err != nil {
+// 		return c.JSON(http.StatusInternalServerError, err)
+// 	}
+// 	return c.JSON(http.StatusOK, &cc)
+// }
+
+// DeletePlotConfiguration delete plot configuration for a project
+func DeletePlotConfiguration(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		pID, err := uuid.Parse(c.Param("project_id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		cID, err := uuid.Parse(c.Param("plot_configuration_id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		if err := models.DeletePlotConfiguration(db, &pID, &cID); err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		return c.JSON(http.StatusOK, make(map[string]interface{}))
 	}
 }
--- models/plot_configuration.go
+++ models/plot_configuration.go
@@ -8,11 +8,12 @@ import (
 	"github.com/lib/pq"
 )
 
-// PlotConfiguration holds information for entity Plot_group
+// PlotConfiguration holds information for entity PlotConfiguration
 type PlotConfiguration struct {
 	ID         uuid.UUID   `json:"id"`
 	Name       string      `json:"name"`
-	ProjectID  uuid.UUID   `json:"project_id" db:"project_id"`
+	Slug       string      `json:"slug"`
+	ProjectID  *uuid.UUID  `json:"project_id" db:"project_id"`
 	Timeseries []uuid.UUID `json:"timeseries" db:"timeseries"`
 	AuditInfo
 }
@@ -61,9 +62,95 @@ func PlotConfigFactory(rows *sqlx.Rows) ([]PlotConfiguration, error) {
 	return pp, nil
 }
 
+// ListPlotConfigurationsSlugs lists used instrument group slugs in the database
+func ListPlotConfigurationsSlugs(db *sqlx.DB) ([]string, error) {
+
+	ss := make([]string, 0)
+	if err := db.Select(&ss, "SELECT slug FROM plot_configuration"); err != nil {
+		return make([]string, 0), err
+	}
+	return ss, nil
+}
+
 // ListPlotConfigurations returns a list of Plot groups
 func ListPlotConfigurations(db *sqlx.DB, projectID *uuid.UUID) ([]PlotConfiguration, error) {
-	var ListPlotConfigurationsSQL = `SELECT id,
+	rows, err := db.Queryx(ListPlotConfigurationsSQL+" WHERE project_id = $1", projectID)
+	if err != nil {
+		return nil, err
+	}
+	return PlotConfigFactory(rows)
+}
+
+// GetPlotConfiguration returns a single plot configuration
+func GetPlotConfiguration(db *sqlx.DB, PID uuid.UUID, CID uuid.UUID) (*PlotConfiguration, error) {
+	var p PlotConfiguration
+
+	row := db.QueryRow(ListPlotConfigurationsSQL+" WHERE project_id = $1 AND id = $2", PID, CID)
+
+	if err := row.Scan(&p.ID, &p.Name, &p.ProjectID, pq.Array(&p.Timeseries), &p.Creator,
+		&p.CreateDate, &p.Updater, &p.UpdateDate); err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
+
+// CreatePlotConfiguration add plot configuration for a project
+func CreatePlotConfiguration(db *sqlx.DB, configurations []PlotConfiguration) ([]IDAndSlug, error) {
+	// Begin the transation
+	txn, err := db.Beginx()
+	if err != nil {
+		return make([]IDAndSlug, 0), err
+	}
+
+	// Prepare the SQL statement
+	stmt, err := txn.Preparex(`INSERT INTO plot_configuration
+		(slug, name, project_id, creator, create_date, updater, update_date) VALUES
+		($1, $2, $3, $4, $5, $6, $7)
+		RETURNING id, slug`)
+
+	// Process the array of configurations
+	cc := make([]IDAndSlug, len(configurations))
+	for idx, c := range configurations {
+		if err := stmt.Get(
+			&cc[idx],
+			c.Slug, c.Name, c.ProjectID, c.Creator, c.CreateDate, c.Updater,
+			c.UpdateDate,
+		); err != nil {
+			return make([]IDAndSlug, 0), err
+		}
+	}
+	// close the statement
+	if err = stmt.Close(); err != nil {
+		return make([]IDAndSlug, 0), err
+	}
+	// Commit the transaction
+	if err = txn.Commit(); err != nil {
+		return make([]IDAndSlug, 0), err
+	}
+	// return the array of id's and slugs
+	return cc, nil
+}
+
+// UpdatePlotConfiguration update plot configuration for a project
+
+// DeletePlotConfiguration delete plot configuration for a project
+func DeletePlotConfiguration(db *sqlx.DB, projectID *uuid.UUID, plotConfigID *uuid.UUID) error {
+	if _, err := db.Exec(
+		`WITH config_id AS (
+			DELETE from plot_configuration where id = $2
+			and project_id = $1 returning id
+		)
+		delete from plot_configuration_timeseries where plot_configuration_id in (select id from config_id)`,
+		projectID, plotConfigID,
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListPlotConfigurationsSQL is the base SQL statement for above functions
+var ListPlotConfigurationsSQL = `SELECT id,
 									name,
 									project_id,
 									t.timeseries,
@@ -77,34 +164,3 @@ func ListPlotConfigurations(db *sqlx.DB, projectID *uuid.UUID) ([]PlotConfigurat
 									FROM plot_configuration_timeseries
 									GROUP BY plot_configuration_id) as t
 									ON id = t.plot_configuration_id`
-
-	rows, err := db.Queryx(ListPlotConfigurationsSQL+" WHERE project_id = $1", projectID)
-	if err != nil {
-		return nil, err
-	}
-	return PlotConfigFactory(rows)
-}
-
-// GetPlotConfiguration returns a single plot configuration
-func GetPlotConfiguration(db *sqlx.DB, ID uuid.UUID) (*PlotConfiguration, error) {
-	var GetPlotConfigurationSQL = `SELECT id,
-									deleted,
-									slug,
-									name,
-									description,
-									project_id,
-									creator,
-									create_date,
-									updater,
-									update_date
-									FROM plot_configuration`
-
-	var g PlotConfiguration
-	if err := db.QueryRowx(
-		GetPlotConfigurationSQL+" WHERE id = $1",
-		ID,
-	).StructScan(&g); err != nil {
-		return nil, err
-	}
-	return &g, nil
-}
