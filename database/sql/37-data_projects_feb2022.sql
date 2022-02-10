bc4e0f2003b450fb1097982e7466a2e1921984b2
Brett Palmberg <29417036+brettpalmberg@users.noreply.github.com>
2022-02-10 08:24:25 -0600

    Add 6 projects: (#126)
    * CTS
    * Chickamauga Lock
    * Kentucky Lock
    * Sardis Dam OK
    * Caesar Creek Dam
    * Olmsted Locks and Dam

---
+++ database/sql/37-data_projects_feb2022.sql
@@ -0,0 +1,7 @@
+INSERT INTO project (id, slug, name, federal_id, image) VALUES
+    ('57261121-e868-41d5-8046-20fabeb930e0','cts','CTS','NIST001','midas-project-default.jpg'),
+    ('c50bee37-011f-4f02-a60b-3ff4baa9b8e2','chickamauga-lock','Chickamauga Lock','TN06504','midas-project-default.jpg'),
+    ('676299fb-5c7a-4a96-95c7-fa81923920f6','kentucky-lock','Kentucky Lock','KY05017','midas-project-default.jpg'),
+    ('f31c5a2c-347e-4534-a002-deae09c87ee6','sardis-dam-ok','Sardis Dam OK','OK22199','midas-project-default.jpg'),
+    ('6756f41b-b5da-41ee-8a0d-d117d6f5b47a','caesar-creek-dam','Caesar Creek Dam','OH00927','midas-project-default.jpg'),
+    ('0bb21182-acfc-4f42-ac0e-9ad8cfc26261','olmsted-locks-and-dam','Olmsted Locks and Dam','IL50745','midas-project-default.jpg');
