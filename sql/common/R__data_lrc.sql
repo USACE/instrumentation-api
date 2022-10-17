07c9801a6f1dd9dc687d2ae1a16e97249cb326f4
krashanoff <leo@krashanoff.com>
2022-10-17 11:24:14 -0700

    Add data dumps, roles, other various required migrations.

---
+++ sql/common/R__data_lrc.sql
@@ -0,0 +1,19 @@
+INSERT INTO project (name, slug, image) VALUES
+    ('Cedars Lock and Dam', 'cedars-lock-and-dam', 'cedars-lock-and-dam.jpg'),
+    ('Chicago River and Harbor Controlling Works', 'chicago-river-and-harbor-controlling-works', 'chicago-river-and-harbor-controlling-works.jpg'),
+    ('Depere Lock and Dam', 'depere-lock-and-dam', 'depere-lock-and-dam.jpg'),
+    ('J. Edward Roush Dam', 'j-edward-roush-dam', 'j-edward-roush-dam.jpg'),
+    ('J. Edward Roush Dam - Markle Levee', 'j-edward-roush-dam-markle-levee', 'j-edward-roush-dam-markle-levee.jpg'),
+    ('Kaukauna Locks and Dam', 'kaukauna-locks-and-dam', 'kaukauna-locks-and-dam.jpg'),
+    ('Little Chute Locks and Dam', 'little-chute-locks-and-dam', 'little-chute-locks-and-dam.jpg'),
+    ('Little Kaukauna Lock and Dam', 'little-kaukauna-lock-and-dam', 'little-kaukauna-lock-and-dam.jpg'),
+    ('Lockport Lock and Controlling Works', 'lockport-lock-and-controlling-works', 'lockport-lock-and-controlling-works.jpg'),
+    ('Lower Appleton Locks and Dam', 'lower-appleton-locks-and-dam', 'lower-appleton-locks-and-dam.jpg'),
+    ('Menasha Lock and Dam', 'menasha-lock-and-dam', 'menasha-lock-and-dam.jpg'),
+    ('Mississinewa Dam', 'mississinewa-dam', 'mississinewa-dam.jpg'),
+    ('Rapide Croche Lock and Dam', 'rapide-croche-lock-and-dam', 'rapide-croche-lock-and-dam.jpg'),
+    ('Salamonie Dam', 'salamonie-dam', 'salamonie-dam.jpg'),
+    ('Thomas J. O''brien Lock and Controlling Works', 'thomas-j-obrien-lock-and-controlling-works', 'thomas-j-obrien-lock-and-controlling-works.jpg'),
+    ('Upper Appleton Locks and Dam', 'upper-appleton-locks-and-dam', 'upper-appleton-locks-and-dam.jpg'),
+    ('Chicago Sanitarty and Ship Canal Dispersal Barrier', 'chicago-sanitary-and-ship-canal-dispersal-barrier', 'chicago-sanitary-and-ship-canal-dispersal-barrier.jpg');
+
