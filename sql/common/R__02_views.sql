77798c269cea1e5ce786b0eb8c694ad5b8905a68
Dennis Smith <smith_dennis@bah.com>
2023-01-13 13:32:50 -0500

    Create flyway scripts

---
+++ sql/common/R__02_views.sql
@@ -0,0 +1,402 @@
+-- -----
+-- Views
+-- -----
+-- v_telemetry
+
+CREATE OR REPLACE VIEW v_instrument_telemetry AS (
+    SELECT a.id,
+           a.instrument_id AS instrument_id,
+           b.id AS telemetry_type_id,
+           b.slug AS telemetry_type_slug,
+           b.name AS telemetry_type_name
+    FROM instrument_telemetry a
+    INNER JOIN telemetry_type b ON b.id = a.telemetry_type_id
+    LEFT JOIN telemetry_goes tg ON a.telemetry_id = tg.id
+    LEFT JOIN telemetry_iridium ti ON a.telemetry_id = ti.id
+);
+
+-- v_instrument
+CREATE OR REPLACE VIEW v_instrument AS (
+    SELECT I.id,
+        I.deleted,
+        S.status_id,
+        S.status,
+        S.status_time,
+        I.slug,
+        I.name,
+        I.type_id,
+        T.name AS type,
+        ST_AsBinary(I.geometry) AS geometry,
+        I.station,
+        I.station_offset,
+        I.creator,
+        I.create_date,
+        I.updater,
+        I.update_date,
+        I.project_id,
+        I.nid_id,
+        I.usgs_id,
+        TEL.telemetry AS telemetry,
+        COALESCE(C.constants, '{}') AS constants,
+        COALESCE(G.groups, '{}') AS groups,
+        COALESCE(A.alert_configs, '{}') AS alert_configs
+    FROM instrument I
+    INNER JOIN instrument_type T ON T.id = I.type_id
+    INNER JOIN (
+        SELECT DISTINCT ON (instrument_id) instrument_id,
+            a.time AS status_time,
+            a.status_id AS status_id,
+            d.name AS status
+        FROM instrument_status a
+        INNER JOIN status d ON d.id = a.status_id
+        WHERE a.time <= now()
+        ORDER BY instrument_id, a.time DESC
+    ) S ON S.instrument_id = I.id
+    LEFT JOIN (
+        SELECT array_agg(timeseries_id) as constants,
+            instrument_id
+        FROM instrument_constants
+        GROUP BY instrument_id
+    ) C on C.instrument_id = I.id
+    LEFT JOIN (
+        SELECT array_agg(instrument_group_id) as groups,
+            instrument_id
+        FROM instrument_group_instruments
+        GROUP BY instrument_id
+    ) G on G.instrument_id = I.id
+    LEFT JOIN (
+        SELECT array_agg(id) as alert_configs,
+            instrument_id
+        FROM alert_config
+        GROUP BY instrument_id
+    ) A on A.instrument_id = I.id
+    LEFT JOIN (
+        SELECT instrument_id,
+                json_agg(
+                    json_build_object(
+                        'id', v.id,
+                        'slug', v.telemetry_type_slug,
+                        'name', v.telemetry_type_name
+                    )
+                ) AS telemetry
+        FROM v_instrument_telemetry v
+        GROUP BY instrument_id
+    ) TEL ON TEL.instrument_id = I.id
+);
+
+-- v_project
+CREATE OR REPLACE VIEW v_project AS (
+    SELECT  p.id,
+            p.federal_id,
+            CASE WHEN p.image IS NOT NULL
+                THEN cfg.static_host || '/projects/' || p.slug || '/images/' || p.image
+                ELSE NULL
+            END AS image,
+            p.office_id,
+            p.deleted,
+            p.slug,
+            p.name,
+            p.creator,
+            p.create_date,
+            p.updater,
+            p.update_date,
+            COALESCE(t.timeseries, '{}') AS timeseries,
+            COALESCE(i.count, 0) AS instrument_count,
+            COALESCE(g.count, 0) AS instrument_group_count
+        FROM project p
+            LEFT JOIN (
+                SELECT project_id,
+                    COUNT(instrument) as count
+                FROM instrument
+                WHERE NOT instrument.deleted
+                GROUP BY project_id
+            ) i ON i.project_id = p.id
+            LEFT JOIN (
+                SELECT project_id,
+                    COUNT(instrument_group) as count
+                FROM instrument_group
+                WHERE NOT instrument_group.deleted
+                GROUP BY project_id
+            ) g ON g.project_id = p.id
+            LEFT JOIN (
+                SELECT array_agg(timeseries_id) as timeseries,
+                    project_id
+                FROM project_timeseries
+                GROUP BY project_id
+            ) t on t.project_id = p.id
+			CROSS JOIN config cfg
+);
+
+-- stored and computed timeseries
+CREATE OR REPLACE VIEW v_timeseries AS (
+    WITH ts_stored_and_computed AS (
+        SELECT
+            id,
+            slug,
+            name,
+            instrument_id,
+            parameter_id,
+            unit_id,
+            (SELECT id IN (SELECT timeseries_id FROM calculation)) AS is_computed
+        FROM timeseries
+    )
+    SELECT t.id                 AS id,
+        t.slug                  AS slug,
+        t.name                  AS name,
+        t.is_computed           AS is_computed,
+        i.slug || '.' || t.slug AS variable,
+        j.id                    AS project_id,
+        j.slug                  AS project_slug,
+        j.name                  AS project,
+        i.id                    AS instrument_id,
+        i.slug                  AS instrument_slug,
+        i.name                  AS instrument,
+        p.id                    AS parameter_id,
+        p.name                  AS parameter,
+        u.id                    AS unit_id,
+        u.name                  AS unit
+    FROM ts_stored_and_computed t
+    INNER JOIN instrument i ON i.id = t.instrument_id
+    INNER JOIN project j ON j.id = i.project_id
+    INNER JOIN parameter p ON p.id = t.parameter_id
+    INNER JOIN unit U ON u.id = t.unit_id
+);
+
+-- stored timeseries only
+CREATE OR REPLACE VIEW v_timeseries_latest AS (
+    SELECT t.*,
+       m.time AS latest_time,
+	   m.value AS latest_value
+    FROM v_timeseries t
+    LEFT JOIN (
+	    SELECT DISTINCT ON (timeseries_id) timeseries_id, time, value
+	    FROM timeseries_measurement
+	    ORDER BY timeseries_id, time DESC
+    ) m ON t.id = m.timeseries_id
+    WHERE NOT t.is_computed
+);
+
+-- computed timeseries and stored dependency timeseries
+CREATE OR REPLACE VIEW v_timeseries_dependency AS (
+    WITH variable_tsid_map AS (
+	    SELECT a.id AS timeseries_id,
+               b.slug || '.' || a.slug AS variable
+	    FROM timeseries a
+	    LEFT JOIN instrument b ON b.id = a.instrument_id
+        WHERE a.id NOT IN (SELECT timeseries_id FROM calculation)
+    )
+    SELECT i.instrument_id   AS instrument_id,
+           i.formula_id      AS timeseries_id,
+           i.parsed_variable AS parsed_variable,
+           m.timeseries_id   AS dependency_timeseries_id
+    FROM (
+        SELECT instrument_id,
+            id AS formula_id,
+            (regexp_matches(contents, '\[(.*?)\]', 'g'))[1] AS parsed_variable
+        FROM timeseries t
+        INNER JOIN calculation cc ON cc.timeseries_id = t.id
+    ) i
+    LEFT JOIN variable_tsid_map m ON m.variable = i.parsed_variable
+);
+
+-- v_timeseries_project_map
+CREATE OR REPLACE VIEW v_timeseries_project_map AS (
+    SELECT t.id AS timeseries_id,
+           p.id AS project_id
+    FROM timeseries t
+    LEFT JOIN instrument n ON t.instrument_id = n.id
+    LEFT JOIN project p ON p.id = n.project_id
+);
+
+CREATE OR REPLACE VIEW v_timeseries_stored AS (
+    SELECT * FROM timeseries WHERE id NOT IN (SELECT timeseries_id FROM calculation)
+);
+
+CREATE OR REPLACE VIEW v_timeseries_computed AS (
+    SELECT
+        ts.*,
+        cc.contents AS contents
+    FROM timeseries ts
+    LEFT JOIN calculation cc ON ts.id = cc.timeseries_id
+    WHERE id IN (SELECT timeseries_id FROM calculation)
+);
+
+-- v_email_autocomplete
+CREATE OR REPLACE VIEW v_email_autocomplete AS (
+    SELECT id,
+           'email' AS user_type,
+	       null AS username,
+	       email AS email,
+           email AS username_email
+    FROM email
+    UNION
+    SELECT id,
+           'profile' AS user_type,
+           username,
+           email,
+           username||email AS username_email
+    FROM profile
+);
+
+-- v_alert
+CREATE OR REPLACE VIEW v_alert AS (
+    SELECT a.id AS id,
+       a.alert_config_id AS alert_config_id,
+       a.create_date AS create_date,
+       p.id AS project_id,
+       p.name AS project_name,
+	   i.id AS instrument_id,
+	   i.name AS instrument_name,
+	   ac.name AS name,
+	   ac.body AS body
+FROM alert a
+INNER JOIN alert_config ac ON a.alert_config_id = ac.id
+INNER JOIN instrument i ON ac.instrument_id = i.id
+INNER JOIN project p ON i.project_id = p.id
+);
+
+CREATE OR REPLACE VIEW v_unit AS (
+    SELECT u.id AS id,
+           u.name AS name,
+           u.abbreviation AS abbreviation,
+           u.unit_family_id AS unit_family_id,
+           f.name           AS unit_family,
+           u.measure_id     AS measure_id,
+           m.name           AS measure
+    FROM unit u
+    INNER JOIN unit_family f ON f.id = u.unit_family_id
+    INNER JOIN measure m ON m.id = u.measure_id
+);
+
+CREATE OR REPLACE VIEW v_plot_configuration AS (
+    SELECT pc.id                                  AS id,
+           pc.slug                                AS slug,
+           pc.name                                AS name,
+           pc.project_id                          AS project_id,
+           t.timeseries_id                        AS timeseries_id,
+           pc.creator                             AS creator,
+           pc.create_date                         AS create_date,
+           pc.updater                             AS updater,
+           pc.update_date                         AS update_date,
+           COALESCE(k.show_masked, 'true')        AS show_masked,
+           COALESCE(k.show_nonvalidated, 'true')  AS show_nonvalidated,
+           COALESCE(k.show_comments, 'true')      AS show_comments,
+           COALESCE(k.auto_range, 'true')         AS auto_range,
+           COALESCE(k.date_range, '1 year')       AS date_range
+    FROM plot_configuration pc
+    LEFT JOIN (
+        SELECT plot_configuration_id    AS plot_configuration_id,
+               array_agg(timeseries_id) AS timeseries_id
+        FROM plot_configuration_timeseries
+        GROUP BY plot_configuration_id
+    ) as t ON pc.id = t.plot_configuration_id
+    LEFT JOIN (
+        SELECT id                AS id,
+               show_masked       AS show_masked,
+               show_nonvalidated AS show_nonvalidated,
+               show_comments     AS show_comments,
+               auto_range        AS auto_range,
+               date_range        AS date_range
+        FROM plot_configuration_settings
+        GROUP BY id
+    ) as k ON pc.id = k.id
+);
+
+-- v_instrument_groups
+CREATE OR REPLACE VIEW v_instrument_group AS (
+    WITH instrument_count AS (
+        SELECT 
+        igi.instrument_group_id,
+        count(igi.instrument_group_id) as i_count 
+        FROM instrument_group_instruments igi
+        JOIN instrument i on igi.instrument_id = i.id and not i.deleted
+        GROUP BY igi.instrument_group_id
+        )
+        ,
+        timeseries_instruments as (
+            SELECT t.id, t.instrument_id, igi.instrument_group_id from timeseries t 
+            JOIN instrument i on i.id = t.instrument_id and not i.deleted
+            JOIN instrument_group_instruments igi on igi.instrument_id = i.id
+        )
+
+        SELECT  ig.id,
+                ig.slug,
+                ig.name,
+                ig.description,
+                ig.creator,
+                ig.create_date,
+                ig.updater,
+                ig.update_date,
+                ig.project_id,
+                ig.deleted,
+                COALESCE(ic.i_count,0) as instrument_count,
+                COALESCE(count(ti.id),0) as timeseries_count
+                --,
+                --COALESCE(count(tm.timeseries_id),0) as timeseries_measurements_count
+                
+        FROM instrument_group ig
+        LEFT JOIN instrument_count ic on ic.instrument_group_id = ig.id
+        LEFT JOIN timeseries_instruments ti on ig.id = ti.instrument_group_id
+        --left join timeseries_measurement tm on tm.timeseries_id = ti.id
+        GROUP BY ig.id, ic.i_count
+        ORDER BY ig.name
+);
+
+-- v_profile_project_roles
+CREATE OR REPLACE VIEW v_profile_project_roles AS (
+    SELECT a.id,
+           a.profile_id,
+           b.edipi,
+           b.username,
+           b.email,
+           b.is_admin,
+           c.id AS project_id,
+           r.id   AS role_id,
+           r.name AS role,
+           UPPER(c.slug || '.' || r.name) AS rolename
+    FROM profile_project_roles a
+    INNER JOIN profile b ON b.id = a.profile_id
+    INNER JOIN project c ON c.id = a.project_id
+    INNER JOIN role    r ON r.id = a.role_id
+    ORDER BY username, role
+);
+
+CREATE OR REPLACE VIEW v_profile AS (
+    WITH roles_by_profile AS (
+        SELECT profile_id,
+               array_agg(UPPER(b.slug || '.' || c.name)) AS roles
+        FROM profile_project_roles a
+        LEFT JOIN project b ON a.project_id = b.id
+        LEFT JOIN role    c ON a.role_id    = c.id
+        GROUP BY profile_id
+    )
+    SELECT p.id,
+           p.edipi,
+           p.username,
+           p.email,
+           p.is_admin,
+           COALESCE(r.roles,'{}') AS roles
+    FROM profile p
+    LEFT JOIN roles_by_profile r ON r.profile_id = p.id
+);
+
+GRANT SELECT ON
+    v_instrument,
+    v_instrument_group,
+    v_plot_configuration,
+    v_project,
+    v_timeseries,
+    v_email_autocomplete,
+    v_alert,
+    v_timeseries_latest,
+    v_timeseries_project_map,
+    v_instrument_telemetry,
+    v_unit,
+    v_profile,
+    v_profile_project_roles,
+    v_timeseries_dependency,
+    v_timeseries_stored,
+    v_timeseries_computed,
+    inclinometer_measurement,
+    v_aware_platform_parameter_enabled
+TO instrumentation_reader;
---
+++ sql/common/V1.0.2__initial_tables_aware.sql
@@ -0,0 +1,124 @@
+drop table if exists 
+    aware_platform_parameter_enabled,
+    aware_platform,
+    aware_parameter;
+
+CREATE TABLE IF NOT EXISTS aware_platform (
+    id UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+    aware_id UUID NOT NULL,
+    instrument_id UUID REFERENCES instrument(id)
+);
+
+CREATE TABLE IF NOT EXISTS aware_parameter (
+    id UUID UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
+    key VARCHAR NOT NULL,
+    parameter_id UUID NOT NULL REFERENCES parameter(id),
+    unit_id UUID NOT NULL REFERENCES unit(id),
+    timeseries_slug VARCHAR NOT NULL,
+    timeseries_name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS aware_platform_parameter_enabled (
+    aware_platform_id UUID NOT NULL REFERENCES aware_platform(id),
+    aware_parameter_id UUID NOT NULL REFERENCES aware_parameter(id),
+    CONSTRAINT aware_platform_unique_parameter UNIQUE(aware_platform_id, aware_parameter_id)
+);
+
+CREATE OR REPLACE VIEW v_aware_platform_parameter_enabled AS (
+    SELECT i.project_id  AS project_id,
+	       i.id          AS instrument_id,
+		   a.aware_id    AS aware_id,
+		   b.key         AS aware_parameter_key,
+		   t.id          AS timeseries_id
+    FROM aware_platform_parameter_enabled e
+    INNER JOIN aware_platform a ON a.id = e.aware_platform_id
+    INNER JOIN instrument i ON i.id = a.instrument_id
+    INNER JOIN aware_parameter b ON b.id = e.aware_parameter_id
+	LEFT JOIN timeseries t ON t.instrument_id=i.id AND t.parameter_id=b.parameter_id AND t.unit_id=b.unit_id
+	ORDER BY i.project_id, a.aware_id
+);
+
+-- Seed Data
+-- aware_parameter
+INSERT INTO aware_parameter (id, key, parameter_id, unit_id, timeseries_slug, timeseries_name) VALUES
+    ('1d9f9d06-6fcb-41dd-9fe4-e513a2575e74', 'depth1', '068b59b0-aafb-4c98-ae4b-ed0365a6fbac', '4ee79a3d-a053-41b8-85b5-bb2eea3c9d1a', 'stage', 'Stage'),
+    ('c5f2842d-a5a9-4f53-9583-f613080a9c36', 'battery', '430e5edb-e2b5-4f86-b19f-cda26a27e151', '6b5bd788-8c78-43bb-b5a3-ad544b858a64', 'battery-voltage', 'Battery Voltage'),
+    ('78d32638-5137-481c-aa9d-a48c2d57824a', 'baro', '1de79e29-fb70-45c3-ae7d-4695517ced90', '55dda9ef-7ba6-4432-b64d-8ef0e65154f4', 'barometric-pressure', 'Barometric Pressure'),
+    ('53ce89a7-1db8-45bd-baf9-9536d75d7046', 'h2oTemp', 'de6112da-8489-4286-ae56-ec72aa09974d', '6462733b-5b42-46a2-ad44-882a5332eafc', 'water-temperature', 'Water Temperature'),
+    ('3ca7c1da-7124-42c0-b92c-f76b5c318b0c', 'rssi', 'b23b141d-ce7b-4e0d-82ab-c8beb39c8325', 'be854f6e-e36e-4bba-9e06-6d5aa09485be', 'signal-strength', 'Signal Strength');
+
+-- Trigger Function; Create Timeseries when aware_platform_parameter_enabled
+CREATE OR REPLACE FUNCTION aware_create_timeseries()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+    AS $$
+BEGIN
+
+INSERT INTO timeseries(instrument_id, parameter_id, unit_id, slug, name) (
+	SELECT a.instrument_id AS instrument_id,
+		   ap.parameter_id AS parameter_id,
+		   ap.unit_id AS unit_id,
+		   ap.timeseries_slug AS slug,
+		   ap.timeseries_name AS name
+	FROM aware_parameter ap
+	CROSS JOIN aware_platform a
+	WHERE ap.id = NEW.aware_parameter_id AND a.id = NEW.aware_platform_id
+)
+ON CONFLICT DO NOTHING;
+RETURN NEW;
+END;
+$$;
+
+-- Trigger; Create Timeseries when aware_platform_parameter_enabled
+CREATE TRIGGER aware_create_timeseries
+AFTER INSERT ON aware_platform_parameter_enabled
+FOR EACH ROW
+EXECUTE PROCEDURE aware_create_timeseries();
+
+
+-- Trigger Function; Enable all AWARE parameters when new record insert into aware_platform
+CREATE OR REPLACE FUNCTION aware_enable_params()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+    AS $$
+BEGIN
+
+INSERT INTO aware_platform_parameter_enabled (aware_platform_id, aware_parameter_id) (
+	SELECT a.id AS aware_platform_id,
+		   b.id AS aware_parameter_id
+	FROM aware_platform a
+	CROSS JOIN aware_parameter b	
+	where a.id = NEW.id
+	ORDER BY aware_platform_id
+	
+)
+ON CONFLICT DO NOTHING;
+RETURN NEW;
+END;
+$$;
+
+-- Trigger; Enable all AWARE parameters when new record insert into aware_platform
+CREATE TRIGGER aware_enable_params
+AFTER INSERT ON aware_platform
+FOR EACH ROW 
+EXECUTE PROCEDURE aware_enable_params();
+
+
+
+-- Seed Data
+
+-- aware_platform
+-- aware_id used below is not an actual aware_id; generated for testing
+INSERT INTO aware_platform (id, aware_id, instrument_id) VALUES
+    ('b896ce34-2bd4-436c-9f28-7a1eefb5744a', '6df213c4-a582-4735-a916-6f4065082872', 'a7540f69-c41e-43b3-b655-6e44097edb7e');
+
+-- enable all parameters for sample aware platform
+INSERT INTO aware_platform_parameter_enabled (aware_platform_id, aware_parameter_id) (
+	SELECT a.id AS aware_platform_id,
+		   b.id AS aware_parameter_id
+	FROM aware_platform a
+	CROSS JOIN aware_parameter b
+	ORDER BY aware_platform_id
+    
+)
+ON CONFLICT DO NOTHING;
