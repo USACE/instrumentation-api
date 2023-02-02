dc52dc3f27691576230b665aad250d7880a3476c
Dennis Smith <smith_dennis@bah.com>
2023-02-02 13:42:36 -0500

    Consolidate test & startup script into compose.sh

--- compose.sh
+++ compose.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-# convenience scipt for starting all containers
 if [ "$1" = "up" ]; then
-    env DOCKER_BUILDKIT=1 docker compose --profile='local' up -d --build;
+    env DOCKER_BUILDKIT=1 docker-compose --profile=local up -d --build;
 elif [ "$1" = "down" ]; then
-    docker compose --profile=local down;
+    docker-compose --profile=local down;
 elif [ "$1" = "clean" ]; then
-    docker compose --profile=local down -v;
+    docker-compose --profile=local down -v;
+elif [ "$1" = "test" ]; then
+    docker-compose up -d --build;
+    if [ "$REPORT" = true ]; then
+        docker run \
+            -v $(pwd)/tests:/etc/newman --network=instrumentation-api_default \
+            --rm \
+            --entrypoint /bin/sh \
+            -t postman/newman \
+            -c "npm i -g newman newman-reporter-htmlextra; \
+                newman run /etc/newman/instrumentation-regression.postman_collection.json \
+                --environment=/etc/newman/postman_environment.docker-compose.json \
+                --reporter-htmlextra-browserTitle 'Instrumentation' \
+                --reporter-htmlextra-title 'Instrumentation Regression Tests' \
+                --reporter-htmlextra-titleSize 4 \
+                -r htmlextra --reporter-htmlextra-export /etc/newman/instrumentation.html"
+    else
+        docker run \
+            --rm \
+            -v $(pwd)/tests:/etc/newman --network=instrumentation-api_default \
+            -t postman/newman run /etc/newman/instrumentation-regression.postman_collection.json \
+            --environment=/etc/newman/postman_environment.docker-compose.json
+    fi
+    docker-compose down;
 else
-    echo -e "usage:\n\t./compose.sh up\n\t./compose.sh down\n\t./compose.sh clean"
+    echo -e "usage:\n\t./compose.sh up\n\t./compose.sh down\n\t./compose.sh clean\n\t./compose.sh test"
 fi
--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script to conveniently git pull and rebuild images and (re)start docker stack
-git pull;
-docker compose --profile=local down;
-docker builder prune --force --filter "label=instrumentation-api";
-docker compose --profile=local up -d --build;
