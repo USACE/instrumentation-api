df62d70139683d01758f758be747a5a3cfe788cf
Dennis Smith <smith_dennis@bah.com>
2023-03-14 19:54:27 -0400

    Update test script for use with dev

--- mock_datalogger/telemetry_post_tester.py
+++ mock_datalogger/telemetry_post_tester.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# CLI tool for mocking POST requests sent from Campbell Scientific data loggers
-
 import argparse
 from datetime import datetime, timedelta
 import time
@@ -9,20 +7,30 @@ import json
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen, Request
 import random
+from getpass import getpass
 
 from typing import Any
 
+# the follow parameters can be set here or overwritten with command line flags
+# this is an api key used local testing. You will be promter for the api key
+# on program start if the --use-mock-api-key flag is not present
+DEFAULT_API_KEY = "8pszF58y7Hpwr8DgR9UYhovcjJYdBhRSMt9dGX1RBmdj6WtH4NUNFao"
+DEFAULT_USE_MOCK_API_KEY = False
 DEFAULT_BASE_URL = "http://telemetry"
 DEFAULT_INTERVAL_SECONDS = 10
-MOCK_API_KEY = "8pszF58y7Hpwr8DgR9UYhovcjJYdBhRSMt9dGX1RBmdj6WtH4NUNFao"
-MODEL = "CR6"
-SN = "12345"
+DEFAULT_MODEL = "CR6"
+DEFAULT_SN = "12345"
+DEFAULT_VERBOSE = False
 
 
-def post_data(url: str, data: str) -> Any | None:
+def post_data(url: str, data: str, api_key: str) -> Any | None:
     json_data = json.dumps(data).encode("utf-8")
 
-    request = Request(url, headers={"Content-Type": "application/json", "X-Api-Key": MOCK_API_KEY}, data=json_data)
+    request = Request(
+        url,
+        headers={"Content-Type": "application/json", "X-Api-Key": api_key},
+        data=json_data,
+    )
 
     try:
         with urlopen(request, timeout=10) as response:
@@ -40,11 +48,11 @@ def post_data(url: str, data: str) -> Any | None:
     return None  # exception was thrown
 
 
-def create_test_data(interval: int, idx: int) -> dict:
+def create_test_data(interval: int, idx: int, model: str, sn: str) -> dict:
     measurement_time_1 = datetime.now().isoformat(timespec="seconds")
-    measurement_time_2 = (
-        datetime.now() - timedelta(seconds=(interval / 2))
-    ).isoformat(timespec="seconds")
+    measurement_time_2 = (datetime.now() - timedelta(seconds=(interval / 2))).isoformat(
+        timespec="seconds"
+    )
 
     return {
         "head": {
@@ -53,10 +61,10 @@ def create_test_data(interval: int, idx: int) -> dict:
             "environment": {
                 "station_name": "6239",
                 "table_name": "Test",
-                "model": MODEL,
-                "serial_no": SN,
-                "os_version": "CR6.Std.12.01",
-                "prog_name": "CPU:Updated_CR6_Sample_Template.CR6",
+                "model": model,
+                "serial_no": sn,
+                "os_version": f"{model}.Std.12.01",
+                "prog_name": f"CPU:Updated_{model}_Sample_Template.{model}",
             },
             "fields": [
                 {
@@ -97,33 +105,70 @@ def create_test_data(interval: int, idx: int) -> dict:
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser("telemetry timeseries uploader")
+    parser = argparse.ArgumentParser(
+        prog="Mock Data Logger",
+        usage="""
+        CLI tool for mocking POST requests sent from Campbell Scientific data loggers.
+        Use default flags for use with local docker compose stack""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
         "--base-url",
-        help="base url to send requests",
+        help=f"base url to send requests to, e.g. stable https://midas-telemetry.rsgis.dev",
         default=DEFAULT_BASE_URL,
         type=str,
     )
     parser.add_argument(
         "--interval",
-        help="interval in seconds to wait between requests",
+        help=f"interval in seconds to wait between requests",
         default=DEFAULT_INTERVAL_SECONDS,
         type=int,
     )
+    parser.add_argument(
+        "--model",
+        help=f"valid model of the registered data logger",
+        default=DEFAULT_MODEL,
+        type=str,
+    )
+    parser.add_argument(
+        "--sn",
+        help=f"valid serial number of the registered data logger",
+        default=DEFAULT_SN,
+        type=str,
+    )
+    parser.add_argument(
+        "--use-mock-api-key",
+        help=f"use default mock api key for local testing",
+        default=DEFAULT_USE_MOCK_API_KEY,
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+    )
+    parser.add_argument(
+        "--verbose",
+        help=f"show outgoing the request's mocked payloads",
+        default=DEFAULT_VERBOSE,
+        type=bool,
+        action=argparse.BooleanOptionalAction,
+    )
     args = parser.parse_args()
 
-    interval = args.interval
-    url = f"{args.base_url}/telemetry/datalogger/{MODEL}/{SN}"
+    if not args.use_mock_api_key:
+        api_key = getpass(prompt="Enter API Key: ")
+    else:
+        api_key = DEFAULT_API_KEY
+
+    url = f"{args.base_url}/telemetry/datalogger/{args.model}/{args.sn}"
 
     i = 0
     while True:
-        data = create_test_data(interval, i)
+        data = create_test_data(args.interval, i, args.model, args.sn)
         i += 1
         print(f"POST: {url}")
-        # print("request payload:", json.dumps(data, indent=2), sep="\n")
-        post_data(url, data)
-        print(f"waiting {interval} seconds...\n")
-        time.sleep(interval)
+        if args.verbose:
+            print("request payload:", json.dumps(data, indent=2), sep="\n")
+        post_data(url, data, api_key)
+        print(f"waiting {args.interval} seconds...\n")
+        time.sleep(args.interval)
 
 
 if __name__ == "__main__":
