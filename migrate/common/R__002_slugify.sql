daf22ba05b2e7a58345a95a3b40f3fd0d9e2ce61
Dennis Smith <smith_dennis@bah.com>
2024-01-10 10:01:56 -0500

    create slugify database function

---
+++ migrate/common/R__002_slugify.sql
@@ -0,0 +1,50 @@
+-- slugify(name, table_name) returns a unique valid slug for text and table name passed.
+-- inspired by
+-- https://gist.github.com/kez/17638bade0382f820280dafa46277435
+-- https://stackoverflow.com/a/10711349/13847311
+
+CREATE EXTENSION IF NOT EXISTS "unaccent";
+
+CREATE OR REPLACE FUNCTION slugify(rawname TEXT, _tbl REGCLASS)
+RETURNS TEXT AS $func$
+DECLARE
+  newslug TEXT := '';
+  n_slugs_taken INTEGER := 0;
+
+BEGIN
+  -- removes accents (diacritic signs) from a given string --
+  WITH unaccented AS (
+    SELECT unaccent(rawname) AS val
+  ),
+  -- lowercases the string
+  lowercased AS (
+    SELECT lower(val) AS val
+    FROM unaccented
+  ),
+  -- remove single and double quotes
+  removed_quotes AS (
+    SELECT regexp_replace(val, '[''"]+', '', 'gi') AS val
+    FROM lowercased
+  ),
+  -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
+  hyphenated AS (
+    SELECT regexp_replace(val, '[^a-z0-9\-_]+', '-', 'gi') AS val
+    FROM removed_quotes
+  ),
+  -- trims hyphens('-') if they exist on the head or tail of the string
+  trimmed AS (
+    SELECT trim(BOTH '-' FROM val) AS val
+    FROM hyphenated
+  )
+  SELECT val INTO newslug FROM trimmed;
+
+  EXECUTE 'SELECT COUNT(*) FROM ' || _tbl  || ' t WHERE t.slug LIKE ''' || newslug || '%''' INTO n_slugs_taken;
+
+  CASE WHEN n_slugs_taken > 0
+  THEN
+    RETURN newslug || '-' || n_slugs_taken::TEXT;
+  ELSE
+    RETURN newslug;
+  END CASE;
+END;
+$func$ LANGUAGE plpgsql;
