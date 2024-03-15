043d3591354c86bd64819fcf849a5444b898e172
Dennis Smith <smith_dennis@bah.com>
2024-03-15 16:20:54 -0400

    bump flyway version

--- build_ib.sh
+++ build_ib.sh
@@ -11,6 +11,6 @@ docker build --build-arg="BASE_IMAGE=registry1.dso.mil/ironbank/docker/scratch:i
 docker build --build-arg="BASE_IMAGE=registry1.dso.mil/ironbank/docker/scratch:ironbank" --target telemetry -t midas-telemetry:$1 api
 docker build --build-arg="BASE_IMAGE=registry1.dso.mil/ironbank/docker/scratch:ironbank" --target alert -t midas-alert:$1 api
 docker build --build-arg="BASE_IMAGE=registry1.dso.mil/ironbank/docker/scratch:ironbank" --target dcs-loader -t midas-dcs-loader:$1 api
-docker build --build-arg="BASE_IMAGE=registry1.dso.mil/ironbank/flyway/flyway-docker:v8.5.9" -t midas-sql:$1 migrate
+docker build --build-arg="BASE_IMAGE=registry1.dso.mil/ironbank/flyway/flyway-docker:v10.9.1" -t midas-sql:$1 migrate
 
 exit 0
