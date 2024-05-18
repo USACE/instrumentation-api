e74b5002b608d181fa6bed6d0cea294b6b5e9fad
Dennis Smith <smith_dennis@bah.com>
2024-05-17 23:12:51 -0400

    containerize openapidoc generation; update to oapi3 spec; use scalar for documentation ui (now served under /docs, formerly /swagger)

---
+++ api/internal/handler/docs.go
@@ -0,0 +1,67 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+const localServer = "http://localhost:8080"
+
+func (h *ApiHandler) CreateDocHtmlHandler(apidoc []byte, serverBaseUrl string, authJwtMocked bool) (echo.HandlerFunc, error) {
+	var apidocJson map[string]interface{}
+	if err := json.Unmarshal(apidoc, &apidocJson); err != nil {
+		return nil, err
+	}
+	url := serverBaseUrl
+	if url == "" {
+		url = localServer
+	}
+	server := map[string]string{"url": url}
+	apidocJson["servers"] = []map[string]string{server}
+
+	newApiDoc, err := json.Marshal(apidocJson)
+	if err != nil {
+		return nil, err
+	}
+	// if running locally, prefill mock bearer token
+	var authOptions string
+	if authJwtMocked {
+		authOptions = mockAuthOptions
+	}
+
+	htmlContent := fmt.Sprintf(htmlTmpl, newApiDoc, authOptions)
+	return func(c echo.Context) error {
+		return c.HTML(http.StatusOK, htmlContent)
+	}, nil
+}
+
+const htmlTmpl = `<!doctype html>
+<html>
+  <head>
+    <title>MIDAS API Reference</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
+    <script
+      id="api-reference"
+      type="application/json"
+    >%s</script>
+    <script>
+      var configuration = {%s theme: 'default', layout: 'classic'}
+      document.getElementById('api-reference').dataset.configuration =
+        JSON.stringify(configuration)
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+  </body>
+</html>`
+
+const mockAuthOptions = `authentication: {
+    preferredSecurityScheme: 'Bearer',
+    apiKey: {
+        token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6IlVzZXIuQXBwbGljYXRpb25BZG1pbiIsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjoyMDAwMDAwMDAwLCJyb2xlcyI6W119.aKaDNBnuhQyXI6zvzn-dAg8SxJSP3mQEx5FTSmJbYog',
+    },
+},`
