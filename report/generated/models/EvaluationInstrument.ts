da06635fcd7287f2a8558c3a204524932915d5fa
Dennis Smith <smith_dennis@bah.com>
2024-05-17 23:14:27 -0400

    remove old generated ts api client

--- report/generated/models/EvaluationInstrument.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type EvaluationInstrument = {
-    instrument_id?: string;
-    instrument_name?: string;
-};
-
