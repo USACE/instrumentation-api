75d5a95ea7f06092b88492edad67c3556f72f4c5
Dennis Smith <smith_dennis@bah.com>
2024-07-01 19:24:57 -0400

    add sm key to env vars

--- report/environment.d.ts
+++ report/environment.d.ts
@@ -7,6 +7,7 @@ declare global {
       AWS_S3_WRITE_TO_BUCKET_PREFIX: string;
       AWS_SM_API_KEY_SECRET_ID: string;
       AWS_SM_ENDPOINT: string;
+      AWS_SM_KEY: string;
       AWS_SM_MOCK_REQUEST: string;
       AWS_SM_REGION: string;
       PUPPETEER_EXECUTABLE_PATH: string;
