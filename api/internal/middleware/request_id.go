a2f3a079e1d09561223aecebb5c09f96aed4d988
Dennis Smith <smith_dennis@bah.com>
2024-07-11 13:44:28 -0400

    use uuid v4 as request id generator

--- api/internal/middleware/request_id.go
+++ api/internal/middleware/request_id.go
@@ -1,11 +1,14 @@
 package middleware
 
 import (
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	echomw "github.com/labstack/echo/v4/middleware"
 )
 
-var reqID = echomw.RequestID()
+var reqID = echomw.RequestIDWithConfig(echomw.RequestIDConfig{
+	Generator: func() string { return uuid.NewString() },
+})
 
 func (m *mw) RequestID(next echo.HandlerFunc) echo.HandlerFunc {
 	return reqID(next)
